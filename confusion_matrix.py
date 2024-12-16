05b8cbeb2ec790748c4c1d57eff295ffe1012afa
Aditya Subramanian <64399367+Avinier@users.noreply.github.com>
2024-12-16 19:05:11 +0530

    Create confusion_matrix.py

---
+++ confusion_matrix.py
@@ -0,0 +1,19 @@
+# 16-12-2024
+# https://www.deep-ml.com/problem/Confusion%20Matrix%20for%20Binary%20Classification
+
+from collections import Counter
+
+def confusion_matrix(data):
+	tuple_data=[]
+	
+	for pair in data:
+	    tuple_data.append(tuple(pair))
+	    
+	counts = Counter(tuple_data)
+	
+	TP, FN, FP, TN = counts[(1, 1)], counts[(1, 0)], counts[(0, 1)],counts[(0, 0)]
+	
+	confusion_matrix = [[TP,FN], [FP, TN]]
+	
+	return confusion_matrix
+	pass
